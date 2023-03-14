9543c1c4c44efc2b5fd00ebbf63f89d5b4791e86
Simon <olav@live.se>
2023-03-14 13:52:28 +0100

    Fixed rating and commenting code

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -5,7 +5,7 @@ import StarBorderIcon from '@mui/icons-material/StarBorder';
 function Rating({ productId, setRating, products }) {
 
   const [product, setProduct] = useState(null);
-  // Letar i products vilken produkt som det gäller från productId.
+  // Letar efter vilken produkt som det gäller från productId.
   useEffect(() => {
     const foundProduct = products.find((product) => product.id === productId);
     setProduct(foundProduct);
@@ -13,26 +13,24 @@ function Rating({ productId, setRating, products }) {
 
   const [hoveredStar, setHoveredStar] = useState(null);
   const filledStars = Math.round(product?.rating ? product.rating : 5);
-  const emptyStars = 5 - filledStars;
-  const filledStarIcons = Array(filledStars).fill(<StarIcon />);
-  const emptyStarIcons = Array(emptyStars).fill(<StarBorderIcon />);
-
+  // Om man trycker på en stjärna, ändra betyget.
   const handleStarClick = (index) => {
     setRating(productId, index + 1);
   }
 
   return (
     <div>
-      {[...filledStarIcons, ...emptyStarIcons].map((icon, index) => {
+      {Array(5).fill().map((_, index) => {
         const isFilled = index < filledStars;
+        const isHovered = index < hoveredStar;
         return (
           <span
             key={index}
-            onMouseEnter={() => setHoveredStar(index)}
+            onMouseEnter={() => setHoveredStar(index + 1)}
             onMouseLeave={() => setHoveredStar(null)}
-            onClick={() => handleStarClick(index)}
+            onClick={() => handleStarClick(index + 1)}
           >
-            {isFilled ? <StarIcon /> : <StarBorderIcon />}
+            {isFilled || isHovered ? <StarIcon /> : <StarBorderIcon />}
           </span>
         )
       })}
